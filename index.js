e428bdd18dddd107e636af26a5a2891df5d6d67d
janiefiteny15 <janiefiteny15@gmail.com>
2025-05-13 10:15:02 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let availableNumbers = [1, 2, 3, 4, 5, 6];
+
+app.get('/', (req, res) => {
+  if (availableNumbers.length === 0) {
+    return res.send('Tất cả các số đã được chọn.');
+  }
+
+  const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+  const selectedNumber = availableNumbers.splice(randomIndex, 1)[0];
+
+  res.send(`Bạn đã nhận được số: <strong>${selectedNumber}</strong>`);
+});
+
+app.listen(port, () => {
+  console.log(`App đang chạy tại http://localhost:${port}`);
+});
